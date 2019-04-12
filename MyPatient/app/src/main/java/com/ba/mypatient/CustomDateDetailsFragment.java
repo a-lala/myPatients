8d718427676b7b490232ce30ebc3a76a7cd27383
aliakeel <aliakeel27@yahoo.com>
2019-04-13 02:09:00 +0430

    add loadfragment function and use it to load medicalfilesfragment

--- MyPatient/app/src/main/java/com/ba/mypatient/CustomDateDetailsFragment.java
+++ MyPatient/app/src/main/java/com/ba/mypatient/CustomDateDetailsFragment.java
@@ -1,13 +1,18 @@
 package com.ba.mypatient;
 
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -24,14 +29,22 @@ public class CustomDateDetailsFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         name = getArguments().getString("name");
         time = getArguments().getString("time");
         view.setBackgroundColor(Color.WHITE);
         ((TextView)view.findViewById(R.id.tv_name)).setText(name);
         ((TextView)view.findViewById(R.id.tv_time)).setText(time);
+        ((ImageView)view.findViewById(R.id.iv_files)).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"hhhh",Toast.LENGTH_SHORT);
+                loadfragment(new MedicalFilesFragment());
+            }
+        });
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,4 +53,16 @@ public class CustomDateDetailsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_custom_date_details, container, false);
     }
 
+//
+Boolean loadfragment(Fragment fragment) {
+    if (fragment != null) {
+        getActivity().getFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
+        return true;
+    }
+    return false;
+}
 }
